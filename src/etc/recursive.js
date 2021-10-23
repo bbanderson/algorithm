7c82f47c02075c589820a62b8bb483604f5e9424
bbanderson <tombyun@naver.com>
2021-10-23 23:47:29 +0900

    Add 재귀함수

--- src/etc/recursive.js
+++ src/etc/recursive.js
@@ -15,11 +15,10 @@ function f2(num) {
 // const result = [1, 2, 3, 4, 5].map((v) => f2(v));
 // console.log(result);
 
-
 function sumElements(arr) {
-  let target = arr.pop()
-  if (!arr.length) return target
-  return target + sumElements(arr)
+  let target = arr.pop();
+  if (!arr.length) return target;
+  return target + sumElements(arr);
 }
 
 // const result = sumElements([1,2,2,2,2,])
@@ -27,7 +26,7 @@ function sumElements(arr) {
 
 /** @param {string} text */
 function isPalindrome(text) {
-  console.log(text)
+  console.log(text);
   if (text.length === 1) {
     return true;
   }
@@ -37,5 +36,32 @@ function isPalindrome(text) {
   return false;
 }
 
-const result = isPalindrome('levelevelevelevel');
+// const result = isPalindrome('levelevelevelevel');
+// console.log(result);
+
+function untilOne(num) {
+  if (num === 1) {
+    return num;
+  }
+  if (num % 2) {
+    console.log(num);
+    return untilOne(3 * num + 1);
+  } else {
+    console.log(num);
+    return untilOne(num / 2);
+  }
+}
+
+// const result = untilOne(3);
+// console.log(result);
+
+function threeCombi(num) {
+  if (num < 1) return 0;
+  if (num === 1) return 1;
+  if (num === 2) return 2;
+  if (num === 3) return 4;
+  return threeCombi(num - 3) + threeCombi(num - 2) + threeCombi(num - 1);
+}
+
+const result = threeCombi(3);
 console.log(result);
